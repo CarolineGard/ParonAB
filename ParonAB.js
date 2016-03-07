db098f353282701654a355135aa4f7b9939d4745
CarolineGard <caroline.gard@hotmail.com>
2016-03-07 08:40:43 +0100

    modified ParonAB project

--- ParonAB.js
+++ ParonAB.js
@@ -1,7 +1,8 @@
 
+//New MongoDB collection. Stores all product data
 Products = new Mongo.Collection("products");
 
-//OPtions for all routes within the project
+//Options for all routes within the project
 Router.configure ({
   layoutTemplate: 'main'
 });
@@ -9,11 +10,6 @@ Router.configure ({
 Router.route('/', {
   name: 'home',
   template: 'home'
-});
-
-Router.route('/form_in', {
-  name: 'form_in',
-  template: 'form_in'
 });
 
 Router.route('/form_out', {
@@ -26,171 +22,152 @@ Router.route('/show_products', {
   template: 'show_products'
 });
 
-
 //Code only runs on the client
 if (Meteor.isClient) {
 
-  Template.show_products.helpers ({
-    
-    'product': function () {
-      return Products.find();
-    }
-  });
-
   Template.home.helpers ({
-
+    //Get selected product in drop down 1
     'showSelectedProduct' : function () {
       var selectedProduct = Session.get('selectedProduct');
-      return Products.findOne(selectedProduct);
+      return selectedProduct;
     },
-
+    //Get selected product in drop down 2
     'showSelectedLocation' : function () {
       var selectedLocation = Session.get('selectedLocation');
-      return Products.findOne(selectedLocation);
-    }
-  });
+      return selectedLocation;
+    },
+    //Get selected radio button
+    'showSelectedRadio' : function () {
+      var selectedRadio = Session.get('selectedRadio');
 
-  Template.drop_down1.events ({
-    'change select' : function (event,template) {
-      Session.set("selectedProduct", event.target.value);
-      console.log(event);
-    }
-  });
-
-  Template.drop_down2.events ({
-    'change select' : function (event,template) {
-      Session.set("selectedLocation", event.target.value);
-      console.log(event);
-    }
-  });
-
-  Template.form_in.events ({
-    //When clicking the submit button
-    'submit form': function(event) {
-      //prevent the browser's default behavior of the event from occurring
-      event.preventDefault();
-
-      var productName = Session.get('selectedProduct');
-      var locationName = Session.get('selectedLocation');
-      var nr_products = parseInt(event.target.nr_products.value);
+      //Returns false for -1 (out), true for 1 (in)
+      if (selectedRadio == -1) 
+        return false; 
       
-      var changingProduct = Products.findOne({name: productName, location: locationName});
-
-      //Update the quantity
-      if (changingProduct) {
-        Products.update(changingProduct._id, {$inc: {quantity: nr_products}});
-      }
-      else {
-        //No product like this!!!
-        //Warning
-      }
-    }
-  });
-
-  Template.form_out.events ({
-    //When clicking the submit button
-    'submit form': function(event) {
-      //prevent the browser's default behavior of the event from occurring
-      event.preventDefault();
-
-      var productName = Session.get('selectedProduct');
-      var locationName = Session.get('selectedLocation');
-      var nr_products = parseInt(event.target.nr_products.value);
-      
-      var changingProduct = Products.findOne({name: productName, location: locationName});
-
-      //Update the quantity
-      if (changingProduct) {
-        Products.update(changingProduct._id, {$inc: {quantity: -nr_products}});
+      else if (selectedRadio == 1)
         return true;
-      }
-      else {
-        //No product like this!!!
-        //Warning
-        return false;
-      }
+
+      else 
+        return true;
     }
   });
 
 
-   Template.drop_down1.helpers ({
+  Template.drop_down1.helpers ({
+    //Drop down for products without repetitions
     'product': function () {
       var myArray = Products.find().fetch();
       var distinctArray = _.uniq(myArray, false, function(d) {return d.name});
       var distinctValues = _.pluck(distinctArray, 'name');
 
       return distinctValues;
-    },
-
-    'setDefault': function () {
-      Session.set("selectedProduct", distinctValues[0]);
     }
   });
 
   Template.drop_down2.helpers ({
+    //Drop down for location without repetitions
     'product': function () {
       var myArray = Products.find().fetch();
       var distinctArray = _.uniq(myArray, false, function(d) {return d.location});
       var distinctValues = _.pluck(distinctArray, 'location');
 
       return distinctValues;
-    },
-
-    'setDefault': function () {
-      Session.set("selectedLocation", distinctValues[0]);
-    },
+    }
   });
 
+  //Returns objects from Collection Products for table 
+  Template.show_products.helpers ({
+    'product': function () {
+      return Products.find();
+    }
+  });
 
-  Template.body.events ({
-    "click #btn-status": function() {
-      //Show the inventory status
-    },
+  Template.drop_down1.events ({
+    'change select' : function (event,template) {
+      //Create a session for the selected product in drop down 1
+      Session.set("selectedProduct", event.target.value);
+      // console.log(event);
+    }
+  });
 
-    'submit .new-report-in': function () {
-      //Prevent default browser form submit
+  Template.drop_down2.events ({
+    'change select' : function (event,template) {
+      //Create a session for the selected location in drop down 2
+      Session.set("selectedLocation", event.target.value);
+      // console.log(event);
+    }
+  });
+
+  Template.home.events ({
+    'change .radios' : function(e) {      
+      var clickedRadio = e.currentTarget;
+      console.log($(clickedRadio).val());
+      
+      //Create a session for the selected radio button
+      Session.set("selctedRadio", $(clickedRadio).val());
+    }
+  });
+
+  Template.form_in.events ({
+    //When clicking the submit button
+    'submit form': function(event, template) {
+      //prevent the browser's default behavior of the event from occurring
       event.preventDefault();
 
-      //Get value from text element
-      var text = event.target.nr_products.value;
+      var productName = Session.get('selectedProduct');
+      var locationName = Session.get('selectedLocation');
+      var nr_products = parseInt(event.target.nr_products.value);
+      
+      //Find which object to update
+      var changingProduct = Products.findOne({name: productName, location: locationName});
 
-      //Get value from drop downs
-      //var product = event.target.
-
-      //Clear form
-      event.target.nr_products.value = "";
-
-
+      //Update the quantity
+      if (changingProduct && nr_products) {
+        Products.update(changingProduct._id, {$inc: {quantity: nr_products}});
+        swal("Inleverans rapporterad!", ""+nr_products+" st "+productName+" tillagda i lager "+locationName);
+        event.target.nr_products.value = "";
+      }
+      else {
+        swal("Kunde inte rapportera leverans!", "Alla fällt måste vara korrekt ifyllda.");
+      }
     }
-    //Obs provar för framtiden från meteortips.com/first-meteor-tutorial/databases-part-2/
-    // "click submit" : function () {
-    //   var selectedProduct = Session.get('selectedProduct');
-    //   Products.update(selectedProduct, {set: });
-    // },
+  });
 
-    // "submit .new-task": function (event) {
-    //   //Prevent default browser form submit
-    //   event.preventDefault();
+  Template.form_out.events ({
+    //When clicking the submit button
+    'submit form': function(event, template) {
+      //prevent the browser's default behavior of the event from occurring
+      event.preventDefault();
 
+      var productName = Session.get('selectedProduct');
+      var locationName = Session.get('selectedLocation');
+      var nr_products = parseInt(event.target.nr_products.value);
+      
+      //Find which object to update
+      var changingProduct = Products.findOne({name: productName, location: locationName});
 
-    //   //Get value from element
-    //   var text = event.target.text.value;
+      //Update the quantity for the object
+      //Check if all form fields are complete
+      if (changingProduct && nr_products) {
+        //Test if the updated quantity < 0 
+        if ( (Products.findOne(changingProduct._id).quantity - nr_products) < 0 ) {
+          swal("Kunde inte rapportera leverans!", "Det finns inte tillräckligt många varor i lager"); 
+        }
+        else {
+          Products.update(changingProduct._id, {$inc: {quantity: -nr_products}});
 
-    //   //insert a task into the collection
-    //   Tasks.insert({
-    //     text: text, createdAt: new Date()
-    //   });
+          swal("Utleverans rapporterad!", ""+nr_products+" st "+productName+" borttagna från lager "+locationName);
+          event.target.nr_products.value = "";
+        }
+      }
+      else {
+        swal("Kunde inte rapportera leverans!", "Alla fällt måste vara korrekt ifyllda.");
+      }
+    }
+  });
+ }
 
-    //   //Clear form
-    //   event.target.text.value = "";
-    // }
-  //});
-   });
-}
-
-
-
-
+//Code only runs on the server
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
